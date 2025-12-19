4d3b537f9dace68ec834ecff84a758dfeb53d558
Hugo.Alves <hfmalves@gmail.com>
2025-12-19 11:26:42 +0000

    mobile menu

--- app/Views/layout/partials_website/cart_drawer.php
+++ app/Views/layout/partials_website/cart_drawer.php
@@ -1,92 +1,110 @@
 <div class="aside aside_right overflow-hidden cart-drawer" id="cartDrawer">
     <div class="aside-header d-flex align-items-center">
-        <h3 class="text-uppercase fs-6 mb-0">SHOPPING BAG ( <span class="cart-amount js-cart-items-count">1</span> ) </h3>
+        <h3 class="text-uppercase fs-6 mb-0">
+            MEU CARRINHO ( <span class="cart-amount js-cart-items-count">1</span> )
+        </h3>
         <button class="btn-close-lg js-close-aside btn-close-aside ms-auto"></button>
-    </div><!-- /.aside-header -->
+    </div>
 
     <div class="aside-content cart-drawer-items-list">
+
         <div class="cart-drawer-item d-flex position-relative">
             <div class="position-relative">
-                <a href="product1_simple.html">
-                    <img loading="lazy" class="cart-drawer-item__img" src="../images/cart-item-1.jpg" alt="">
+                <a href="<?= base_url('product/zessi-dresses') ?>">
+                    <img loading="lazy" class="cart-drawer-item__img"
+                         src="<?= base_url('images/cart-item-1.jpg') ?>" alt="">
                 </a>
             </div>
             <div class="cart-drawer-item__info flex-grow-1">
-                <h6 class="cart-drawer-item__title fw-normal"><a href="product1_simple.html">Zessi Dresses</a></h6>
-                <p class="cart-drawer-item__option text-secondary">Color: Yellow</p>
-                <p class="cart-drawer-item__option text-secondary">Size: L</p>
+                <h6 class="cart-drawer-item__title fw-normal">
+                    <a href="<?= base_url('product/zessi-dresses') ?>">Zessi Dresses</a>
+                </h6>
+                <p class="cart-drawer-item__option text-secondary">Cor: Amarelo</p>
+                <p class="cart-drawer-item__option text-secondary">Tamanho: L</p>
+
                 <div class="d-flex align-items-center justify-content-between mt-1">
                     <div class="qty-control position-relative">
-                        <input type="number" name="quantity" value="1" min="1" class="qty-control__number border-0 text-center">
+                        <input type="number" name="quantity" value="1" min="1"
+                               class="qty-control__number border-0 text-center">
                         <div class="qty-control__reduce text-start">-</div>
                         <div class="qty-control__increase text-end">+</div>
-                    </div><!-- .qty-control -->
-                    <span class="cart-drawer-item__price money price">$99</span>
+                    </div>
+                    <span class="cart-drawer-item__price money price">€99</span>
                 </div>
             </div>
 
             <button class="btn-close-xs position-absolute top-0 end-0 js-cart-item-remove"></button>
-        </div><!-- /.cart-drawer-item d-flex -->
+        </div>
 
         <hr class="cart-drawer-divider">
 
         <div class="cart-drawer-item d-flex position-relative">
             <div class="position-relative">
-                <a href="product1_simple.html">
-                    <img loading="lazy" class="cart-drawer-item__img" src="../images/cart-item-2.jpg" alt="">
+                <a href="<?= base_url('product/kirby-tshirt') ?>">
+                    <img loading="lazy" class="cart-drawer-item__img"
+                         src="<?= base_url('images/cart-item-2.jpg') ?>" alt="">
                 </a>
             </div>
             <div class="cart-drawer-item__info flex-grow-1">
-                <h6 class="cart-drawer-item__title fw-normal"><a href="product1_simple.html">Kirby T-Shirt</a></h6>
-                <p class="cart-drawer-item__option text-secondary">Color: Black</p>
-                <p class="cart-drawer-item__option text-secondary">Size: XS</p>
+                <h6 class="cart-drawer-item__title fw-normal">
+                    <a href="<?= base_url('product/kirby-tshirt') ?>">Kirby T-Shirt</a>
+                </h6>
+                <p class="cart-drawer-item__option text-secondary">Cor: Preto</p>
+                <p class="cart-drawer-item__option text-secondary">Tamanho: XS</p>
+
                 <div class="d-flex align-items-center justify-content-between mt-1">
                     <div class="qty-control position-relative">
-                        <input type="number" name="quantity" value="4" min="1" class="qty-control__number border-0 text-center">
+                        <input type="number" name="quantity" value="4" min="1"
+                               class="qty-control__number border-0 text-center">
                         <div class="qty-control__reduce text-start">-</div>
                         <div class="qty-control__increase text-end">+</div>
-                    </div><!-- .qty-control -->
-                    <span class="cart-drawer-item__price money price">$89</span>
+                    </div>
+                    <span class="cart-drawer-item__price money price">€89</span>
                 </div>
             </div>
 
             <button class="btn-close-xs position-absolute top-0 end-0 js-cart-item-remove"></button>
-        </div><!-- /.cart-drawer-item d-flex -->
+        </div>
 
         <hr class="cart-drawer-divider">
 
         <div class="cart-drawer-item d-flex position-relative">
             <div class="position-relative">
-                <a href="product1_simple.html">
-                    <img loading="lazy" class="cart-drawer-item__img" src="../images/cart-item-3.jpg" alt="">
+                <a href="<?= base_url('product/cableknit-shawl') ?>">
+                    <img loading="lazy" class="cart-drawer-item__img"
+                         src="<?= base_url('images/cart-item-3.jpg') ?>" alt="">
                 </a>
             </div>
             <div class="cart-drawer-item__info flex-grow-1">
-                <h6 class="cart-drawer-item__title fw-normal"><a href="product1_simple.html">Cableknit Shawl</a></h6>
-                <p class="cart-drawer-item__option text-secondary">Color: Green</p>
-                <p class="cart-drawer-item__option text-secondary">Size: L</p>
+                <h6 class="cart-drawer-item__title fw-normal">
+                    <a href="<?= base_url('product/cableknit-shawl') ?>">Xale Cableknit</a>
+                </h6>
+                <p class="cart-drawer-item__option text-secondary">Cor: Verde</p>
+                <p class="cart-drawer-item__option text-secondary">Tamanho: L</p>
+
                 <div class="d-flex align-items-center justify-content-between mt-1">
                     <div class="qty-control position-relative">
-                        <input type="number" name="quantity" value="3" min="1" class="qty-control__number border-0 text-center">
+                        <input type="number" name="quantity" value="3" min="1"
+                               class="qty-control__number border-0 text-center">
                         <div class="qty-control__reduce text-start">-</div>
                         <div class="qty-control__increase text-end">+</div>
-                    </div><!-- .qty-control -->
-                    <span class="cart-drawer-item__price money price">$129</span>
+                    </div>
+                    <span class="cart-drawer-item__price money price">€129</span>
                 </div>
             </div>
 
             <button class="btn-close-xs position-absolute top-0 end-0 js-cart-item-remove"></button>
-        </div><!-- /.cart-drawer-item d-flex -->
+        </div>
 
-    </div><!-- /.aside-content -->
+    </div>
 
     <div class="cart-drawer-actions position-absolute start-0 bottom-0 w-100">
         <hr class="cart-drawer-divider">
         <div class="d-flex justify-content-between">
             <h6 class="fs-base fw-medium">SUBTOTAL:</h6>
-            <span class="cart-subtotal fw-medium">$176.00</span>
-        </div><!-- /.d-flex justify-content-between -->
-        <a href="shop_cart.html" class="btn btn-light mt-3 d-block">View Cart</a>
-        <a href="shop_checkout.html" class="btn btn-primary mt-3 d-block">Checkout</a>
-    </div><!-- /.aside-content -->
-</div><!-- /.aside -->
+            <span class="cart-subtotal fw-medium">€176,00</span>
+        </div>
+        <a href="<?= base_url('cart') ?>" class="btn btn-light mt-3 d-block">Ver Carrinho</a>
+        <a href="<?= base_url('checkout') ?>" class="btn btn-primary mt-3 d-block">Finalizar Compra</a>
+    </div>
+</div>
--- app/Views/layout/partials_website/footer_mobile.php
+++ app/Views/layout/partials_website/footer_mobile.php
@@ -1,29 +1,31 @@
-
-<!-- Mobile Fixed Footer -->
 <footer class="footer-mobile container w-100 px-5 d-md-none bg-body">
     <div class="row text-center">
         <div class="col-4">
-            <a href="../index.html" class="footer-mobile__link d-flex flex-column align-items-center">
-                <svg class="d-block" width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg"><use href="#icon_home" /></svg>
-                <span>Home</span>
+            <a href="<?= base_url() ?>" class="footer-mobile__link d-flex flex-column align-items-center">
+                <svg class="d-block" width="18" height="18" viewBox="0 0 18 18">
+                    <use href="#icon_home" />
+                </svg>
+                <span>Início</span>
             </a>
-        </div><!-- /.col-3 -->
-
+        </div>
         <div class="col-4">
-            <a href="../index.html" class="footer-mobile__link d-flex flex-column align-items-center">
-                <svg class="d-block" width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg"><use href="#icon_hanger" /></svg>
-                <span>Shop</span>
+            <a href="<?= base_url('shop') ?>" class="footer-mobile__link d-flex flex-column align-items-center">
+                <svg class="d-block" width="18" height="18" viewBox="0 0 18 18">
+                    <use href="#icon_hanger" />
+                </svg>
+                <span>Loja</span>
             </a>
-        </div><!-- /.col-3 -->
-
+        </div>
         <div class="col-4">
-            <a href="../index.html" class="footer-mobile__link d-flex flex-column align-items-center">
+            <a href="<?= base_url('user/account/wishlist') ?>" class="footer-mobile__link d-flex flex-column align-items-center">
                 <div class="position-relative">
-                    <svg class="d-block" width="18" height="18" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><use href="#icon_heart" /></svg>
+                    <svg class="d-block" width="18" height="18" viewBox="0 0 20 20">
+                        <use href="#icon_heart" />
+                    </svg>
                     <span class="wishlist-amount d-block position-absolute js-wishlist-count">3</span>
                 </div>
-                <span>Wishlist</span>
+                <span>Favoritos</span>
             </a>
-        </div><!-- /.col-3 -->
-    </div><!-- /.row -->
-</footer><!-- /.footer-mobile container position-fixed d-md-none bottom-0 -->
+        </div>
+    </div>
+</footer>
